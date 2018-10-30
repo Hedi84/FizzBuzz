1173254ac3ef448d77237eb9925183089cf223fe
Hedi Van Meurs <alisoun@gmail.com>
2018-10-30 17:23:33 +0000

    only 13

--- index.js
+++ index.js
@@ -18,49 +18,153 @@ function printNumbers (array) {
   newArray = [];
   const length = array.length;
   for (let i = 0; i < length; i++) {
-    number = fizzBuzz(array[i]);
+    number = isWord(array[i]);
+    // newNumber = reverse(number)
     newArray.push(number);
   }
   console.log(newArray);
 }
 
-// checks if the number is a multiple of 3 and 5(FizzBuzz), 3(Fizz), or 5 (FizzBuzz)
-function fizzBuzz (number) {
-    if (number % 3 === 0 && number % 5 === 0) {
-      return fezz(number, 'FizzBuzz')
-    } else if (number % 7 === 0 && number % 3 === 0) {
-      return fezz(number, 'FizzBang')
-    } else if (number % 7 === 0 && number % 5 === 0) {
-      return fezz(number, 'BuzzBang')
-    } else if (number % 11 === 0) {
-      return fezz(number, 'Bong')
-    } else if (number % 13 === 0) {
-      return "Fezz"
-    } else if (number % 3 === 0) {
-      return fezz(number, 'Fizz')
-    } else if (number % 5 === 0) {
-      return fezz(number, 'Buzz')
-    } else if (number % 7 === 0) {
-      return fezz(number, 'Bang')
-    } else {
-      return number
-    }
+// checks if the number is a multiple
+// function multiple(number, multiple) {
+//   if (number % multiple === 0) {
+//     return true
+//   } else {
+//     return false
+//   }
+// }
+// returns number or word
+function isWord (number) {
+    // var pickedNumbers = [3, 5, 7, 11, 13, 17]
+    // for (let i = 0; i < pickedNumbers.length; i++) {
+      // let multiple = pickedNumbers[i]
+      // if (multiple(number, multiple) === true) {
+      let string = ""
+      string = three(number, string);
+      string = five(number, string);
+      string = seven(number, string);
+      string = eleven(number, string);
+      string = seventeen(number,string);
+      if (string === "") {
+        return number
+      }  else {
+        return string
+      }
 }
 
-// check if the number is also a multiple of 13 and apply fezz rules
-function fezz (number, word) {
-  if (number % 13 === 0 && word === 'Fizz') {
-    return 'FizzFezz'
-  } else if (number % 13 === 0 && word === 'FizzBuzz') {
-    return 'FizzFezzBuzz'
-  } else if (number % 13 === 0 && word === 'FizzBang') {
-    return 'FizzFezzBang'
-  } else if (number % 13 === 0 ) {
-    return word + 'Fezz'
+// function threeAndFive (number) {
+//   if (number % 3 === 0 && number % 5 === 0) {
+//     return 'FizzBuzz';
+//   } else {
+//     return false;
+//   }
+// }
+//
+// function sevenAndThree (number) {
+//   if (number % 7 === 0 && number % 3 === 0) {
+//     return 'FizzBang';
+//   } else {
+//     return false;
+//   }
+// }
+//
+// function sevenAndFive (number) {
+//   if (number % 7 === 0 && number % 5 === 0) {
+//     return 'BuzzBang';
+//   } else {
+//     return false;
+//   }
+// }
+
+function eleven (number, string) {
+if (number % 11 === 0) {
+    string = 'Bong';
+  }
+  return string
+}
+
+function three (number, string) {
+  if (number % 3 === 0) {
+    string = string + 'Fizz';
+  }
+  return string
+}
+
+
+function five (number, string) {
+  if (number % 5 === 0) {
+    string = string + 'Buzz';
+  }
+  return string
+}
+
+function seven (number, string) {
+  if (number % 7 === 0) {
+    string = string + 'Bang';
+  }
+  return string
+}
+
+function thirteen (number) {
+  if (number % 13 === 0) {
+    return 'Fezz';
   } else {
-    return word
+    return false;
   }
 }
+
+function seventeen (number, string) {
+  if (number % 17 === 0 && string !== "") {
+    array = string.match(/.{1,4}/g);
+    reversedArray = array.reverse();
+    string = reversedArray.join("");
+  }
+  return string
+}
+//
+// function fizzBuzz(number) {
+//   if (threeAndFive(number) !== false) {
+//     return threeAndFive(number)
+//   } else if (five(number) !== false) {
+//     return five(number);
+//   }else if (three(number) !== false) {
+//     return three(number);
+//   }
+// }
+//
+// // check if the number is also a multiple of 13 and apply fezz rules
+// function fezz (number, word) {
+//   if (number % 13 === 0 && word === 'Fizz') {
+//     return 'FizzFezz'
+//   } else if (number % 13 === 0 && word === 'FizzBuzz') {
+//     return 'FizzFezzBuzz'
+//   } else if (number % 13 === 0 && word === 'FizzBang') {
+//     return 'FizzFezzBang'
+//   } else if (number % 13 === 0 ) {
+//     return 'Fezz' + word
+//   } else {
+//     return word
+//   }
+// }
+//
+// function reverse (number, word) {
+//   if (number % 17 === 0) {
+//     array = word.split();
+//     reversedArray = array.reverse();
+//     return reversedArray.join("");
+//   } else {
+//     return word
+//   }
+// }
+
+// pick mulitple numbers
+// function pickNumbers () {
+//   pickedNumbers = []
+//   while (input !== c) {
+//
+//
+//   }
+// }
 
 // run program
 function runProgram () {
